d0e0568182568b72ae88929a16b69fa0d72f9080
dzrootstack <dzrootstack@outlook.com>
2025-03-21 14:08:30 -0400

    update the content

--- src/renderer/components/modals/EditDeviceModal.tsx
+++ src/renderer/components/modals/EditDeviceModal.tsx
@@ -150,7 +150,7 @@ class EditDeviceModal extends React.Component<Props, State> {
               If you did not write it down, you can still get it back, but this
               requires some digging : more info on&nbsp;
               <a
-                href="https://github.com/BSoDium/Slashboard/#readme"
+                href="https://github.com/dzrootstack/monitoring-desktop-client/#readme"
                 target="_blank"
                 rel="noreferrer"
               >
